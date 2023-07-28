3064619b21df35ca35e2a04bbd677b293fce2af2
Neha Balamurugan <90981845+nbalamur03@users.noreply.github.com>
2023-07-28 11:58:01 +0200

    vanilla carla client scripts

---
+++ PythonAPI/carla/agents/navigation/behavior_types.py
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+""" This module contains the different parameters sets for each behavior. """
+
+
+class Cautious(object):
+    """Class for Cautious agent."""
+    max_speed = 40
+    speed_lim_dist = 6
+    speed_decrease = 12
+    safety_time = 3
+    min_proximity_threshold = 12
+    braking_distance = 6
+    tailgate_counter = 0
+
+
+class Normal(object):
+    """Class for Normal agent."""
+    max_speed = 50
+    speed_lim_dist = 3
+    speed_decrease = 10
+    safety_time = 3
+    min_proximity_threshold = 10
+    braking_distance = 5
+    tailgate_counter = 0
+
+
+class Aggressive(object):
+    """Class for Aggressive agent."""
+    max_speed = 70
+    speed_lim_dist = 1
+    speed_decrease = 8
+    safety_time = 3
+    min_proximity_threshold = 8
+    braking_distance = 4
+    tailgate_counter = -1
+=======
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+""" This module contains the different parameters sets for each behavior. """
+
+
+class Cautious(object):
+    """Class for Cautious agent."""
+    max_speed = 40
+    speed_lim_dist = 6
+    speed_decrease = 12
+    safety_time = 3
+    min_proximity_threshold = 12
+    braking_distance = 6
+    tailgate_counter = 0
+
+
+class Normal(object):
+    """Class for Normal agent."""
+    max_speed = 50
+    speed_lim_dist = 3
+    speed_decrease = 10
+    safety_time = 3
+    min_proximity_threshold = 10
+    braking_distance = 5
+    tailgate_counter = 0
+
+
+class Aggressive(object):
+    """Class for Aggressive agent."""
+    max_speed = 70
+    speed_lim_dist = 1
+    speed_decrease = 8
+    safety_time = 3
+    min_proximity_threshold = 8
+    braking_distance = 4
+    tailgate_counter = -1
+>>>>>>> 00e23bff4de5381bc60fcd2b0230a8a7c6a0d72d
